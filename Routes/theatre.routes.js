e03359784f2019846f03b0b14762a38be3330463
Ankit kumar <ak8068716@gmail.com>
2023-02-01 14:18:08 +0530

    token admin verfication added into theater

--- Routes/theatre.routes.js
+++ Routes/theatre.routes.js
@@ -1,12 +1,13 @@
 const  express = require('express');
 const theatreController = require('../controller/theatre.controller')
 const route = express.Router();
-route.post("/mba/api/v1/theatres", theatreController.createTheatre);
-route.get("/mba/api/v1/theatres", theatreController.getAllTheatres);
-route.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
-route.put("/mba/api/v1/theatres/:id", theatreController.updateTheatre);
-route.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
-route.put("/mba/api/v1/theatres/:theatreId/movies", theatreController.addMoviesToTheatre)
-route.get("/mba/api/v1/theatres/:theatreId/movies/:movieId",theatreController.checkIfMovieRunningInTheatre);
+const {verifyToken,isAdmin} = require('../middlewares/authJWT');
+route.post("/mba/api/v1/theatres",[verifyToken,isAdmin], theatreController.createTheatre);
+route.get("/mba/api/v1/theatres",[verifyToken], theatreController.getAllTheatres);
+route.get("/mba/api/v1/theatres/:id",[verifyToken],theatreController.getTheatre);
+route.put("/mba/api/v1/theatres/:id",[verifyToken,isAdmin], theatreController.updateTheatre);
+route.delete("/mba/api/v1/theatres/:id",[verifyToken,isAdmin], theatreController.deleteTheatre);
+route.put("/mba/api/v1/theatres/:theatreId/movies",[verifyToken,isAdmin], theatreController.addMoviesToTheatre)
+route.get("/mba/api/v1/theatres/:theatreId/movies/:movieId",[verifyToken],theatreController.checkIfMovieRunningInTheatre);
 
 module.exports = route;
