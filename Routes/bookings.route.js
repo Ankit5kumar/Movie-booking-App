662865fa55e68644e60f33efedf0afe30169ef4f
Ankit kumar <ak8068716@gmail.com>
2023-02-05 22:58:23 +0530

    booking Routes added

--- Routes/bookings.route.js
+++ Routes/bookings.route.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const Route = express.Router();
-const {verifyToken} = require('../middlewares/authJWT');
+const {verifyToken,isAdmin} = require('../middlewares/authJWT');
 const validateBooking = require('../middlewares/validateBooking');
 const bookingController = require('../controller/booking.controller');
 
 Route.post("/mba/api/v1/bookings",[verifyToken, validateBooking],bookingController.createBooking)
 Route.get("/mba/api/v1/bookings/:id",[verifyToken],bookingController.getBookingById)
 Route.get("/mba/api/v1/bookings",[verifyToken],bookingController.getallBookings)
+Route.put("/mba/api/v1/bookings/:id/cancel",[verifyToken],bookingController.cancelBooking)
+
+
 
 module.exports = Route
