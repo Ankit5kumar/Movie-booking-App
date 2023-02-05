27f3e7095caa43d4816f997e89e34334c9693a88
Ankit kumar <ak8068716@gmail.com>
2023-02-05 22:30:40 +0530

    Booking controller added

--- controller/booking.controller.js
+++ controller/booking.controller.js
@@ -1,5 +1,6 @@
 const Booking = require("../models/booking.model")
 const User = require("../models/user.model")
+const constants =  require('../utils/constants')
 
 exports.createBooking = async (req,res) => {
     const user  = await User.findOne({userId:req.userId})
@@ -33,3 +34,44 @@ exports.getallBookings = async (req,res)=>{
  res.status(200).send({msg:"here's the bookings",bookings})
 }
 
+exports.updateBooking = async (req, res) => {
+    const savedBooking = await Booking.findById({_id:req.params.id})
+    if(!savedBooking){
+        res.status(400).send({msg:"Invalid Id"})
+    }
+
+    savedBooking.theatreId = req.body.theatreId ? req.body.theatreId : savedBooking.theatreId;
+    savedBooking.movieId = req.body.movieId ? req.body.movieId : savedBooking.movieId;
+    savedBooking.userId = req.body.userId ? req.body.userId : savedBooking.userId;
+    savedBooking.timing = req.body.timing ? req.body.timing : savedBooking.timing;
+    savedBooking.noOfSeats = req.body.noOfSeats ? req.body.noOfSeats : savedBooking.noOfSeats;
+    savedBooking.totalCost = savedBooking.noOfSeats * constants.ticketPrice;
+    savedBooking.status = req.body.status ? req.body.status : savedBooking.status;
+
+    try {
+        const updatedBooking = await savedBooking.save();
+        res.status(201).send(updatedBooking)
+    } catch (error) {
+        res.status(500).send({msg:"internal error"})
+    }
+}
+
+exports.cancelBooking = async (req, res) => {
+    const savedBooking = await savedBooking.findOne({_id:req.params.id});
+    const savedUser = await User.findOne({
+        userId:req.userId
+    })
+    if(!savedBooking){
+        return res.status(400).send("Invalid Booking Id")
+    }
+    if(!savedBooking.userId.equals(savedUser._id)){
+        return res.status(403).send("User has insufficient permissions to cancel this booking")
+    }
+    savedBooking.status=constants.bookingStatus.cancelled;
+    try {
+        const updatedooking = await savedBooking.save()
+        res.status(201).send(updatedooking)
+    } catch (error) {
+        res.status(500).send({message:"Internal Error while updating the booking "+e.message});
+    }
+}
